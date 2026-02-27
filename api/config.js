bed4444f7536ce84b3b106cb5c514ed951069e5d
nycricketStream <kumar19usa@gmail.com>
2026-02-27 10:52:57 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -6,10 +6,10 @@ const redis = Redis.fromEnv();
 export default async function handler(req, res) {
 
   // 🔐 PROTECTION GOES HERE
-  if (req.method === 'POST') {
+  /*if (req.method === 'POST') {
     if (req.headers.authorization !== `Bearer ${process.env.ADMIN_SECRET}`) {
       return res.status(401).json({ error: "Unauthorized" });
-    }
+    }*/
 
     await redis.set('broadcast-config', req.body);
     return res.status(200).json({ ok: true });
